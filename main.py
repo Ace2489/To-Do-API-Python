499bf6820630aca2939b05a5d8d992560809e790
Covenant <okoroaforcovenant99@gmail.com>
2022-06-11 12:29:35 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from models import User
+
+app = FastAPI()
+
+Users:list[User] = [User(name = "John Paul", email = "mahio4u@yahoo.com", password = 1234, age = 23)]
+
+@app.get("/users")
+def get_users():
+    return Users
+
+@app.post("/user/register")
+def register_user(user:User):
+    Users.append(user)
+
+@app.delete("/user/me")
+def delete_user(user:User):
+    Users.remove(user)  
+
