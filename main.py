2d95b2bb92b26b0e4be7c43cd57deed261e9d830
Covenant <okoroaforcovenant99@gmail.com>
2022-06-11 19:43:37 +0100

    Added unique IDs to each user

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ app = FastAPI()
 
 Users:list[User] = [User(name = "John Paul", email = "mahio4u@yahoo.com", password = 1234, age = 23)]
 
+
 @app.get("/users")
 def get_users():
     return Users
@@ -17,3 +18,6 @@ def register_user(user:User):
 def delete_user(user:User):
     Users.remove(user)  
 
+@app.post("/task")
+def new_task():
+    pass
--- models.py
+++ models.py
@@ -1,7 +1,16 @@
+from uuid import UUID, uuid4
 from pydantic import BaseModel
+
 
 class User(BaseModel):
     name:str
     email:str
     password:str
     age:int
+    id: UUID = uuid4()
+
+class Task(BaseModel):
+    #id, Completed_stats
+    id:UUID = uuid4()
+
+
