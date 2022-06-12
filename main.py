7b0072f65d5a37bdca4c1ad1955e190e63ee44d7
Covenant <okoroaforcovenant99@gmail.com>
2022-06-12 23:19:42 +0100

    House cleaning and formatting to fit guidelines

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
-from hashlib import new
-from tkinter import N
 from uuid import UUID
 from fastapi import FastAPI
 from models import User, Task, NewTaskType, Status
 
 app = FastAPI()
 
-Users:list[User] = [User(name = "John Paul", email = "mahio4u@yahoo.com", password = 1234, age = 23)]
-Tasks:list[Task] = [Task(id = "56bc791c-11ea-4a18-9769-374d675f1270",
-    desc = "reading book",
-    status = Status.N)]
+Users: list[User] = [User(name="John Paul", email="mahio4u@yahoo.com", password=1234, age=23)]
+Tasks: list[Task] = [Task(id="56bc791c-11ea-4a18-9769-374d675f1270",
+                          desc="reading book",
+                          status=Status.N)]
+
 
 @app.get("/users")
 def get_users():
     return Users
 
+
 @app.post("/user/register")
-def register_user(user:User):
+def register_user(user: User):
     Users.append(user)
 
+
 @app.delete("/user/moi")
-def delete_user(user:User):
-    Users.remove(user)  
+def delete_user(user: User):
+    Users.remove(user)
+
 
 @app.post("/task")
-def new_task(task:NewTaskType):
-    new_task = Task(desc = task.description)
+def new_task(task: NewTaskType):
+    new_task = Task(desc=task.description)
     Tasks.append(new_task)
+
 
 @app.get("/task")
 def get_tasks():
     return Tasks
+
 
 @app.get("/task/{task_id}")
 def get_task_by_id(task_id: UUID):
--- models.py
+++ models.py
@@ -1,26 +1,27 @@
 from enum import Enum
-from tkinter import N
-from typing import Optional
 from uuid import UUID, uuid4
+
 from pydantic import BaseModel
 
 
 class User(BaseModel):
-    name:str
-    email:str
-    password:str
-    age:int
+    name: str
+    email: str
+    password: str
+    age: int
     id: UUID = uuid4()
+
 
 class Status(str, Enum):
     N = "Not Completed"
     Y = "Completed"
 
+
 class Task(BaseModel):
-    id:UUID = uuid4()
-    desc:str  
-    status:Status = Status.N
-    #Having issues getting the addition of tasks using description only, also the enum wahala
+    id: UUID = uuid4()
+    desc: str
+    status: Status = Status.N
+
 
 class NewTaskType(BaseModel):
-    description: str 
+    description: str
